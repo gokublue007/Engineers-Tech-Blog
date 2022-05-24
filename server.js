e39073d88afc0c84293fee3d0aecd6d243c0e135
Elias Vasquez <eliasvasquez7@gmail.com>
2022-05-24 01:10:05 -0600

    Added all files needed to start layout for web page

--- server.js
+++ server.js
@@ -16,3 +16,40 @@ const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+//* CREATE SESSION OBJECT
+const sessionObj = {
+    secret: process.env.DB_SESSION_SECRET,
+    cookie: {
+        maxAge: 3000000
+    },
+    resave: false,
+    saveUninitialized: true,
+    //* CREATE SEQUELIZE STORE FOR SESSION
+    store: new SequelizeStore({
+        db: sequelize,
+    }),
+};
+
+  //* INSTANTIATE SESSION MIDDLEWARE
+    app.use(session(sessionObj));
+
+  //* BRING IN HELPERS
+    const hbs = exphbs.create({helpers});
+
+  //* START UP HANDLEBARS ENGINE AND INSTANTIATE VIEWS
+    app.engine('handlebars', hbs.engine);
+    app.set('view engine', 'handlebars');
+    
+  //* INSTANTIATE TYPICAL EXPRESS MIDDLEWARE
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.static(path.join(__dirname, 'public')));
+
+  //* MIDDLEWARE FOR THE ROUTES
+    app.use(routes);
+
+  //* OPEN PORT AND OPEN SEQUELIZE COMMUNICATION
+    sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
