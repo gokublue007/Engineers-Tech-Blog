952b303228dd83566e0c003d4c6b3deecbed0650
Elias Vasquez <eliasvasquez7@gmail.com>
2022-05-24 00:47:40 -0600

    Adding connection to db

---
+++ config/connection.js
@@ -0,0 +1,24 @@
+//* import sequelize and env secrets
+const Sequelize = require("sequelize");
+require("dotenv").config();
+
+//* if there is a connection to heroku, get the sequelize object to link to the heroku mysql setup
+let sequelize;
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  //* otherwise connect to local mysql database
+    sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: "localhost",
+        dialect: "mysql",
+        port: 3306,
+    }
+);
+}
+
+//* export the config connection
+module.exports = sequelize;
