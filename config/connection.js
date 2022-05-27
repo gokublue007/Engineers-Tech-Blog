2f20f13ec77a3460562871e8b4f4e1d755efcd7d
Elias Vasquez <eliasvasquez7@gmail.com>
2022-05-26 18:33:03 -0600

    Adding connection

--- config/connection.js
+++ config/connection.js
@@ -2,27 +2,23 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-sequelize = new Sequelize('mysql://qows6mm77xk6o0j4:y8iy8uxioeub3nin@ebh2y8tqym512wqs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ni4uw583fwhj3vw7');
-
-// //* if there is a connection to heroku, get the sequelize object to link to the heroku mysql setup
-// let sequelize;
-// if (process.env.JAWSDB_URL) {
-//   sequelize = new Sequelize(process.env.JAWSDB_URL);
-// } else {
-//   //* otherwise connect to local mysql database
-//   sequelize = new Sequelize(
-//     process.env.DB_NAME,
-//     process.env.DB_USER,
-//     process.env.DB_PASSWORD,
-//     {
-//       host: 'localhost',
-//       dialect: 'mysql',
-//       port: 3306
-//     }
-//   );
-// };
-
-// mysql://qows6mm77xk6o0j4:y8iy8uxioeub3nin@ebh2y8tqym512wqs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ni4uw583fwhj3vw7
+//* if there is a connection to heroku, get the sequelize object to link to the heroku mysql setup
+let sequelize;
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  //* otherwise connect to local mysql database
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+};
 
 //* export the config connection
 module.exports = sequelize;
